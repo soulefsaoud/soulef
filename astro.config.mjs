0d1d012d51e08cebd69dd5b981e647fa88f3fbb4
soulef77 <saoud.soulef@laposte.net>
2026-03-27 15:58:30 +0100

    add: github pages deployment workflo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://soulef.github.io',
+    // Si le repo ne s'appelle PAS votre-username.github.io :
+    // base: '/nom-du-repo',
+})
