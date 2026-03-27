1a95c9b7dd72338757a20e7ec1211a5c116b77b1
soulef77 <saoud.soulef@laposte.net>
2026-03-27 16:13:07 +0100

    fix astro base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://soulef.github.io',
-    // Si le repo ne s'appelle PAS votre-username.github.io :
-    // base: '/nom-du-repo',
-})
+        site: 'https://soulefsaoud.github.io',
+        base: '/soulef/',
+ })
